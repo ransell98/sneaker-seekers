a0bd69edcfcd8eba230c0f0e18e506b5bb6720b4
sengvang <sengv74@gmail.com>
2022-02-17 13:51:38 -0600

    committing changes to App class

--- server/src/main/java/learn/sneaker_seekers/App.java
+++ server/src/main/java/learn/sneaker_seekers/App.java
@@ -1,4 +1,34 @@
 package learn.sneaker_seekers;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        // Configure CORS globally versus
+        // controller-by-controller or method-by-method.
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("DELETE", "GET", "POST", "PUT", "OPTIONS")
+                        .allowedOrigins("*");
+            }
+        };
+    }
 }
