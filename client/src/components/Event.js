66ceef95cb531663cb1a6df84a6ef6f6f2397838
Sean Mussack <seanmussack@gmail.com>
2022-02-18 16:08:47 -0600

    updated events page, fleshed out individual event page

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,12 +1,212 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Button, Image } from "react-bootstrap";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+
+import Page from "./Page";
+import Loading from "./Loading";
+import ErrorCard from "./ErrorCard";
+import TableCard from "./TableCard";
+//testing only
+import { EVENTS } from "./Events";
+
+//testing only
+const TABLES = [
+    {"table_id": 1,
+    "status": "Booked",
+    "event_id": 1,
+    "app_user": {
+        "app_user_id": 1,
+        "username": "alice_in_chains"
+    },
+    "listings": [
+
+    ]},
+    {"table_id": 2,
+    "status": "Booked",
+    "event_id": 1,
+    "app_user": {
+        "app_user_id": 2,
+        "username": "BobTheSlob99"
+    },
+    "listings": [
+
+    ]},
+    {"table_id": 3,
+    "status": "Booked",
+    "event_id": 1,
+    "app_user": {
+        "app_user_id": 3,
+        "username": "xmascarol63"
+    },
+    "listings": [
+
+    ]},
+]
 
 function Event() {
     const { id: eventId } = useParams();
+    const [thisEvent, setThisEvent] = useState();
+    const [isEventLoading, setIsEventLoading] = useState(true);
+    const [tables, setTables] = useState([]);
+    const [isTablesLoading, setIsTablesLoading] = useState(true);
 
+    useEffect(() => {
+        fetchEvent();
+        fetchTables();
+    }, []);
+
+    function fetchEvent() {
+        return new Promise(() => {
+            delay(1000)
+            .then(() => {
+                for (var i = 0; i < EVENTS.length; i++) {
+                    if (EVENTS[i].event_id === eventId) {
+                        setThisEvent(EVENTS[i]);
+                    }
+                }
+            })
+            .then(() => {
+                setIsEventLoading(false);
+            });
+        })
+    }
+
+    function fetchTables() {
+        return new Promise(() => {
+            delay(3000)
+            .then(() => {
+                setTables(TABLES);
+            })
+            .then(() => {
+                setIsTablesLoading(false);
+            });
+        })
+    }
+
+    //testing only
+    function delay(t, v) {
+        return new Promise(function(resolve) {
+            setTimeout(resolve.bind(null, v), t)
+        });
+    }
+
+    function handleBookTable() {
+        console.log("handleBookTable");
+    }
+
+    function renderEventImage() {
+        return (
+            <>
+                {thisEvent.image
+                ? <Image 
+                    src={`../${thisEvent.image}`}
+                    style={{
+                        "maxHeight": "300px", 
+                        "maxWidth": "100%"
+                    }}
+                />
+                : <></>}
+            </>
+        );
+    }
+
+    function renderEventLocation() {
+        return (
+            <div className="mt-4">
+                <h3><strong>Location:</strong></h3>
+                <h4>{thisEvent.location.name}</h4>
+                <p>
+                    {thisEvent.location.address}
+                    <br/>
+                    {thisEvent.location.city}
+                </p>
+            </div>
+        );
+    }
+
+    function renderEventDate() {
+        return (
+            <div className="mt-4">
+                <h3><strong>Date:</strong></h3>
+                <h4>
+                    {thisEvent.event_date.toLocaleString("default", {weekday: "long"})}{" "}
+                    {thisEvent.event_date.toLocaleString("default", {month: "long"})}{" "}
+                    {thisEvent.event_date.getDate()}{", "}
+                    {thisEvent.event_date.getFullYear()}
+                </h4>
+            </div>
+        )
+    }
+
+    function renderTables() {
+        return (
+            <div className="mt-4 mb-5">
+                <h3><strong>Tables:</strong></h3>
+                {isTablesLoading
+                ? <Loading/>
+                : <>
+                    {tables && tables.length > 0
+                    ? <>
+                        <h4>
+                            Available Tables: {thisEvent.num_table - tables.length}/{thisEvent.num_table}
+                            {thisEvent.num_table - tables.length > 0
+                            ? <>{renderBookTableButton()}</>
+                            : <></>}
+                        </h4>
+                        <ul className="ps-0">
+                            {tables.map((table) => {
+                                return (
+                                    <TableCard
+                                        table={table}
+                                        key={table.table_id}
+                                    />
+                                );
+                            })}
+                        </ul>
+                    </>
+                    : <ErrorCard message={"Problem fetching tables."}/>
+                    }
+                </>}
+            </div>
+        )
+    }
+
+    function renderBookTableButton() {
+        return (
+            <Button variant="primary" onClick={handleBookTable} className="ms-3">
+                Book Table
+                <FontAwesomeIcon 
+                    icon={faCirclePlus} 
+                    className="ms-2"
+                />
+            </Button>
+        );
+    }
+    
     return (
-        <>
-            <h1>Event #{eventId}</h1>
-        </>
+        <Page title={
+            (isEventLoading || !thisEvent)
+            ? "View Event"
+            : thisEvent.event_name
+        }>
+            {isEventLoading
+            ? <Loading/>
+            : <>
+                {thisEvent
+                ? <>
+                    {renderEventImage()}
+                    {renderEventLocation()}
+                    {renderEventDate()}
+                    {renderTables()}
+                </>
+                : <ErrorCard message={"Event not found."}/>
+                }
+            </>
+            }
+        </Page>
     );
 }
 
--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -2,38 +2,64 @@ import { useState, useEffect } from "react";
 import { Col, Dropdown, Row } from "react-bootstrap";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSort, faSortAlphaAsc, faSortAlphaDesc,  faSortNumericAsc, faSortNumericDesc } from '@fortawesome/free-solid-svg-icons';
+import { faSortAlphaAsc, faSortAlphaDesc,  faSortNumericAsc, faSortNumericDesc } from '@fortawesome/free-solid-svg-icons';
 
 import Page from "./Page";
 import EventCard from "./EventCard";
 import Loading from "./Loading";
 
-const EVENTS = [
+//testing only
+export const EVENTS = [
     {"event_id": "1",
     "event_name": "Sneaker Seekers Atlanta 2022",
-    "event_date": "2022-03-05",
-    "event_location": "Atlanta",
-    "num_table": 50},
+    "event_date": new Date("2022-03-05"),
+    "location": {
+        "name": "Atlanta Convention Center",
+        "address": "240 Peachtree Street NW Suite 2200",
+        "city": "Atlanta",
+    },
+    "num_table": 50,
+    "image": "images/Placeholder.png"},
     {"event_id": "2",
     "event_name": "Sneaker Seekers Austin 2022",
-    "event_date": "2022-04-16",
-    "event_location": "Austin",
-    "num_table": 36},
+    "event_date": new Date("2022-04-16"),
+    "location": {
+        "name": "Austin Convention Center",
+        "address": "500 E. Cesar Chavez St.",
+        "city": "Austin",
+    },
+    "num_table": 36,
+    "image": "images/Placeholder.png"},
     {"event_id": "3",
     "event_name": "Sneaker Seekers Minneapolis 2022",
-    "event_date": "2022-03-26",
-    "event_location": "Minneapolis",
-    "num_table": 72},
+    "event_date": new Date("2022-03-26"),
+    "location": {
+        "name": "Minneapolis Convention Center",
+        "address": "1301 Second Avenue South",
+        "city": "Minneapolis",
+    },
+    "num_table": 72,
+    "image": "images/Placeholder.png"},
     {"event_id": "4",
     "event_name": "Sneaker Seekers Denver 2022",
-    "event_date": "2022-04-03",
-    "event_location": "Denver",
-    "num_table": 47},
+    "event_date": new Date("2022-04-03"),
+    "location": {
+        "name": "Denver Convention Center",
+        "address": "700 14th St.",
+        "city": "Denver",
+    },
+    "num_table": 47,
+    "image": "images/Placeholder.png"},
     {"event_id": "5",
     "event_name": "ShoeCon Atlanta 2022",
-    "event_date": "2022-03-16",
-    "event_location": "Atlanta",
-    "num_table": 42},
+    "event_date": new Date("2022-03-16"),
+    "location": {
+        "name": "Atlanta Convention Center",
+        "address": "240 Peachtree Street NW Suite 2200",
+        "city": "Atlanta",
+    },
+    "num_table": 42,
+    "image": "images/Placeholder.png"},
 ]
 
 function Events() {
@@ -61,25 +87,21 @@ function Events() {
         switch (sortByIndex) {
             case 0:
                 return sortByDate(a, b);
-                break;
             case 1:
                 return sortByDate(b, a);
-                break;
             case 2:
                 return sortByLocation(a, b);
-                break;
             case 3:
                 return sortByLocation(b, a);
-                break;
         }
     }
 
     function sortByDate(a, b) {
-        return ("" + a.event_date).localeCompare(b.event_date);
+        return a.event_date - b.event_date;
     }
 
     function sortByLocation(a, b) {
-        return ("" + a.event_location).localeCompare(b.event_location);
+        return ("" + a.location.name).localeCompare(b.location.name);
     }
 
     function renderSortByDropdown() {
@@ -118,9 +140,9 @@ function Events() {
             {isLoading
             ? <Loading/>
             : <>
-                <Row>
+                <Row className="w-100 mt-3">
                     <Col
-                        md={{offset: 8}}
+                        md={{offset: 2}}
                     >
                         {renderSortByDropdown()}
                     </Col>
