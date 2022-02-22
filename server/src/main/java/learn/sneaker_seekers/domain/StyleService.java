dcdd9f4adff99cc7c25f6a1c17c4dcf555100f39
Ryan Ansell <ransell98@gmail.com>
2022-02-22 16:42:09 -0600

    finished controllers

--- server/src/main/java/learn/sneaker_seekers/domain/StyleService.java
+++ server/src/main/java/learn/sneaker_seekers/domain/StyleService.java
@@ -1,7 +1,9 @@
 package learn.sneaker_seekers.domain;
 
+import learn.sneaker_seekers.data.DataAccessException;
 import learn.sneaker_seekers.data.EventRepository;
 import learn.sneaker_seekers.data.StyleRepository;
+import learn.sneaker_seekers.models.Listing;
 import learn.sneaker_seekers.models.Style;
 import org.springframework.stereotype.Service;
 
@@ -12,4 +14,25 @@ public class StyleService {
     public StyleService(StyleRepository repository) { this.repository = repository; }
 
     public Style findByStyleId(int styleId) { return repository.findByStyleId(styleId); }
+
+    public Result add(Style style) {
+        Result result = validate(style);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        style = repository.add(style);
+        result.setPayload(style);
+        return result;
+    }
+
+    private Result validate(Style style) {
+        Result result = new Result();
+
+        if (style == null) {
+            result.addErrorMessage("Style cannot be null.");
+        }
+
+        return result;
+    }
 }
